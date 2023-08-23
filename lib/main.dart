a7f41d47d12b7f0b9767cff785ff955cf83abb1f
Safa <safaanwar2211@gmail.com>
2023-08-23 13:47:45 -0700

    3 commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'Screens/splash_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -13,13 +15,10 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-
-
-      //home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const SplashScreen(),
     );
   }
 }
