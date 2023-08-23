48732473bfcd8aff6e12c0f3489a31fe664f09fc
Safa <safaanwar2211@gmail.com>
2023-08-23 15:08:22 -0700

    5 commit

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -10,6 +10,22 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        automaticallyImplyLeading: false,
+        title: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Flutter',style: TextStyle(
+              fontFamily: 'Pacifico',fontSize: 30,color: Colors.red,
+            ),),
+            Text('NewsApp  ',style: TextStyle(
+              fontFamily: 'Pacifico',fontSize: 30,color: Colors.black,
+            ),),
+          ],
+        ),
+      ),
+    );
   }
 }
