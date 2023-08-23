3d0f75ceb0bc3aed98873d8bd6fed50aa0861755
Safa <safaanwar2211@gmail.com>
2023-08-23 16:56:04 -0700

    6 commit

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -1,6 +1,4 @@
-
 import 'package:flutter/material.dart';
-
 import 'category_screen.dart';
 
 class HomeScreen extends StatefulWidget {
@@ -17,22 +15,56 @@ class _HomeScreenState extends State<HomeScreen> {
       appBar: AppBar(
         centerTitle: true,
         automaticallyImplyLeading: false,
-        leading: IconButton(onPressed: () { 
-          Navigator.push(context, MaterialPageRoute(builder: (context)=>CategoryScreen()));
-        }, icon: Image.asset('images/category_icon.png'),
-          
+        leading: IconButton(
+          onPressed: () {
+            Navigator.push(context,
+                MaterialPageRoute(builder: (context) => CategoryScreen()));
+          },
+          icon: Image.asset('images/category_icon.png'),
         ),
         title: const Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('Flutter',style: TextStyle(
-              fontFamily: 'Pacifico',fontSize: 30,color: Colors.red,
-            ),),
-            Text('NewsApp  ',style: TextStyle(
-              fontFamily: 'Pacifico',fontSize: 30,color: Colors.black,
-            ),),
+            Text(
+              'Flutter',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 30,
+                color: Colors.red,
+              ),
+            ),
+            Text(
+              'NewsApp  ',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
           ],
         ),
+        actions: [
+          PopupMenuButton(
+              icon: Icon(Icons.more_vert),
+              itemBuilder: (context)=>[
+                const PopupMenuItem(
+                    value: 1,
+                    child: Text('BBC news')),
+                const PopupMenuItem(
+                    value: 2,
+                    child: Text('Al-Jazeera-english news')),
+                const PopupMenuItem(
+                    value: 3,
+                    child: Text('CNN news')),
+                const PopupMenuItem(
+                    value: 4,
+                    child: Text('Independent news')),
+                const PopupMenuItem(
+                    value: 5,
+                    child: Text('Ary news')),
+              ]),
+
+        ],
       ),
     );
   }
