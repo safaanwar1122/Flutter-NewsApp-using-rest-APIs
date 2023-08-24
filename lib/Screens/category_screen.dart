d875ec0e51de92ce4793926e927f5e8252401d9b
Safa <safaanwar2211@gmail.com>
2023-08-23 17:25:16 -0700

    6 commit

--- lib/Screens/category_screen.dart
+++ lib/Screens/category_screen.dart
@@ -9,6 +9,90 @@ class CategoryScreen extends StatefulWidget {
 class _CategoryScreenState extends State<CategoryScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      appBar: AppBar(
+
+        title: const Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              'Flutter',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 30,
+                color: Colors.red,
+              ),
+            ),
+            Text(
+              'NewsApp  ',
+              style: TextStyle(
+                fontFamily: 'Pacifico',
+                fontSize: 30,
+                color: Colors.black,
+              ),
+            ),
+          ],
+        ),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+           Row(
+             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+             children: [
+               ElevatedButton(onPressed: (){
+
+               }, child: const Text('Sports', style: TextStyle(
+                 fontFamily: 'Pacifico',fontSize: 18,fontWeight: FontWeight.bold,
+                 color: Colors.black,
+               ),),
+               ),
+               ElevatedButton(onPressed: (){
+
+               }, child: const Text('Health', style: TextStyle(
+                 fontFamily: 'Pacifico',fontSize: 18,fontWeight: FontWeight.bold,
+                 color: Colors.black,
+               ),),
+               ),
+               ElevatedButton(onPressed: (){
+
+               }, child: const Text('Entertainment', style: TextStyle(
+                 fontFamily: 'Pacifico',fontSize: 18,fontWeight: FontWeight.bold,
+                 color: Colors.black,
+               ),),
+
+               ),
+               ElevatedButton(onPressed: (){
+
+               }, child: Text('Technology', style: TextStyle(
+                 fontFamily: 'Pacifico',fontSize: 18,fontWeight: FontWeight.bold,
+                 color: Colors.black,
+               ),),
+
+               ),
+               ElevatedButton(onPressed: (){
+
+               }, child: const Text('General', style: TextStyle(
+                 fontFamily: 'Pacifico',fontSize: 18,fontWeight: FontWeight.bold,
+                 color: Colors.black,
+               ),),
+
+               ),
+               ElevatedButton(onPressed: (){
+
+               }, child: const Text('Business', style: TextStyle(
+                 fontFamily: 'Pacifico',fontSize: 18,fontWeight: FontWeight.bold,
+                 color: Colors.black,
+               ),),
+
+               ),
+             ],
+           ),
+
+          ],
+        ),
+      ),
+    );
   }
 }
---
+++ lib/practice_apis.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class PacticeAPI extends StatefulWidget {
+  const PacticeAPI({Key? key}) : super(key: key);
+
+  @override
+  State<PacticeAPI> createState() => _PacticeAPIState();
+}
+
+class _PacticeAPIState extends State<PacticeAPI> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+
+        ],
+      ),
+    );
+  }
+}
